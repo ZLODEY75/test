8afd8c75ef2d5a636dff311ecfebec48ff8825a8
ZLODEY75 <121172171+ZLODEY75@users.noreply.github.com>
2024-07-03 11:15:04 +0300

    Update main.go

--- main.go
+++ main.go
@@ -7,26 +7,22 @@ import (
 
 func home_page(page http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(page, "gg")
-
 }
 func contacts_page(page http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(page, "cont")
-
 }
 func donate_page(page http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(page, "donate")
-
 }
 
 func handleRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/contacts/", contacts_page)
 	http.HandleFunc("/donate/", donate_page)
-	http.ListenAndServe(":8080", nil)
 
+	http.ListenAndServe(":8080", nil)
 }
 
 func main() {
 	handleRequest()
-
 }
