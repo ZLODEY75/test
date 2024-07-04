25e7312a9564b06c47178c85ea77bd33f3fa68ba
ZLODEY75 <121172171+ZLODEY75@users.noreply.github.com>
2024-07-04 13:56:16 +0300

    Update main.go

--- main.go
+++ main.go
@@ -5,8 +5,25 @@ import (
 	"net/http"
 )
 
+type User struct {
+	name                 string
+	age                  uint16
+	money                int16
+	avg_grads, happiness float64
+}
+
+func (u *User) getAllinfo() string {
+	return fmt.Sprintf("User name is: %s He is %d and he has meney: %d", u.name, u.age, u.money)
+}
+func (u *User) setNewName(newName string) {
+	u.name = newName
+}
+
 func home_page(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "gg")
+	bob := User{"bob", 25, -50, 4.2, 0.8}
+	bob.setNewName("Alex")
+	fmt.Fprintf(page, bob.getAllinfo())
+
 }
 func contacts_page(page http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(page, "cont")
@@ -24,5 +41,7 @@ func handleRequest() {
 }
 
 func main() {
+	//bob := User{name: "bob", age: 25, money: -50, avg_grads: 4.2, happiness: 0.8}
+
 	handleRequest()
 }
