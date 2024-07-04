3d0daf9a5dfa8e35328b7cad86f292104eb0120a
ZLODEY75 <121172171+ZLODEY75@users.noreply.github.com>
2024-07-04 14:25:39 +0300

    Update

--- main.go
+++ main.go
@@ -2,27 +2,31 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
 type User struct {
-	name                 string
-	age                  uint16
-	money                int16
-	avg_grads, happiness float64
+	Name                 string
+	Age                  uint16
+	Money                int16
+	Avg_grads, Happiness float64
+	Hoobbies             []string
 }
 
 func (u *User) getAllinfo() string {
-	return fmt.Sprintf("User name is: %s He is %d and he has meney: %d", u.name, u.age, u.money)
+	return fmt.Sprintf("User name is: %s He is %d and he has meney: %d", u.Name, u.Age, u.Money)
 }
 func (u *User) setNewName(newName string) {
-	u.name = newName
+	u.Name = newName
 }
 
-func home_page(page http.ResponseWriter, r *http.Request) {
-	bob := User{"bob", 25, -50, 4.2, 0.8}
-	bob.setNewName("Alex")
-	fmt.Fprintf(page, bob.getAllinfo())
+func home_page(w http.ResponseWriter, r *http.Request) {
+	bob := User{"bob", 25, -50, 4.2, 0.8, []string{"Footbboll", "Skate", "Danse"}}
+	//bob.setNewName("Alex")
+	//fmt.Fprintf(page, "<b>Main text<b>")
+	tmpl, _ := template.ParseFiles("templetes/home_page.html")
+	tmpl.Execute(w, bob)
 
 }
 func contacts_page(page http.ResponseWriter, r *http.Request) {
